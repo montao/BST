c8e3a42dd54d9ce30fb8ea77943c243ff881c290
Niklas Rosencrantz <niklas@montao.com.br>
2017-11-22 23:42:26 +0100

    Update bst.c

--- bst.c
+++ bst.c
@@ -66,7 +66,7 @@ int main() {
     while (T-- > 0) {
         //printf("Input data. %d:", T);
         //scanf("%d %d", &data, &node_id);
-        int r = rand() % 100; node id
+        int r = rand() % 100; //node id
         r2 = T* (rand() % 100); // data
         root = insert_node(root, r2, r, "foobar");
     }
@@ -76,6 +76,6 @@ int main() {
    // scanf("%d", &T);
     T = r2;
     printf("node data %d %s", find_node_data(root, T)->data, find_node_data(root, T)->name);
-      printf("node data %d %s", find_node_data(NULL, T)->data, find_node_data(NULL, T)->name);
+    printf("node data %d %s", find_node_data(NULL, T)->data, find_node_data(NULL, T)->name);
     return 0;
 }
